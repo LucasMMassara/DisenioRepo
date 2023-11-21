e396249e528dbb6247bebbb22476fc1770dc646f
LucasMMassara <lukislopkm@gmail.com>
2023-11-21 02:09:19 -0300

    Sql para buscar por numcliente

--- src/main/java/com/mycompany/intentandohaceralgoconmivida/IntentandoHacerAlgoConMiVida.java
+++ src/main/java/com/mycompany/intentandohaceralgoconmivida/IntentandoHacerAlgoConMiVida.java
@@ -1,8 +1,11 @@
 package com.mycompany.intentandohaceralgoconmivida;
 
 import GUI.App;
+import daos.DAOCliente;
 import daos.DAOPais;
 import gestores.GestorPais;
+import java.util.List;
+import logica.Cliente;
 import logica.Domicilio;
 import logica.EstadoCivil;
 import logica.Localidad;
@@ -25,34 +28,9 @@ public class IntentandoHacerAlgoConMiVida {
         cddp.cargarPackPaisesLocIndic();
         */
         
+        /*
         App aplicacion = new App();
         aplicacion.inicioApp();
-        
-        
-        //String calle, String numero, String depto, String piso
-        /*Domicilio dom = new Domicilio("San Martin","3584","0","0");
-        
-        DomicilioJpaController domjpa = new DomicilioJpaController();
-        
-        LocalidadJpaController locjpa = new LocalidadJpaController();
-        
-        Localidad loc = locjpa.findLocalidad(3);
-        System.out.println(loc.toString());
-        
-        dom.setLocalidad(loc);
-        
-        try{
-            domjpa.create(dom);
-        }
-        catch(Exception e){
-            System.out.println("Error al cargar en la BBDD");
-        }
-        
-        /*
-        GestorPais gp = new GestorPais();
-        for (Pais p: gp.ObtenerPaises()){
-            System.out.println(p.toString());
-        }
         */
         
     }
--- src/main/java/daos/DAOCliente.java
+++ src/main/java/daos/DAOCliente.java
@@ -1,22 +1,13 @@
 package daos;
 
-import java.util.ArrayList;
 import logica.Cliente;
 import java.util.List;
-import dto.ClienteDTO;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import logica.TipoDocumento;
+import javax.persistence.Query;
 import persistenciajpa.ClienteJpaController;
 import static util.EntityManagerUtil.getEntityManager;
 
 
 public class DAOCliente extends DAOAbstract<Cliente> {
-
-    public int obtenerCantClientesPorPais(int idPais) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
     
     public void guardarCliente(Cliente c){
         
@@ -28,6 +19,15 @@ public class DAOCliente extends DAOAbstract<Cliente> {
             System.out.println("Error al cargar el cliente en la BBDD" +  e.getMessage());
         }
 
+    }
+    
+    public List<Cliente> obtenerClientePorNumCliente(String numCliente){
+        
+        String stringquery = "SELECT c FROM " + Cliente.class.getName() + " c WHERE c.numCliente = ?1";
+        Query q = getEntityManager().createQuery(stringquery);
+        q.setParameter(1, numCliente);
+        return q.getResultList();
+        
     }
 
 }
