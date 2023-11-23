f350df2baf7d9c6af217045746b18e0c2d80832c
LucasMMassara <lukislopkm@gmail.com>
2023-11-23 01:32:55 -0300

    Facilitandole la vida al que hace la interfaz

--- src/main/java/gestores/GestorMarca.java
+++ src/main/java/gestores/GestorMarca.java
@@ -3,53 +3,113 @@ package gestores;
 import java.util.List;
 
 import daos.DAOMarca;
+import logica.AnioFabricacion;
 import logica.Marca;
+import logica.Modelo;
 
 public class GestorMarca {
-	
-	//Capaz hay que traer todo y ir filtrando a medida que elije el usuario y no hacerlo de esta manera.
-	
-	public List<Marca> obtenerMarcas(){
-		
-		return (new DAOMarca()).obtenerAll();
-	}
 
-        /*
-        
-        GestorPais gp = new GestorPais();
-        listaPaises = gp.ObtenerPaises();    
-        listaProvincias = listaPaises.get(0).getProvincias();
-        listaLocalidades = listaProvincias.get(0).getLocalidades();
-        
-        String[] paises = new String[listaPaises.size()];
-        for (int i = 0; i < listaPaises.size(); i++) {
-            paises[i] = listaPaises.get(i).getNombre();
+    public List<Marca> marcasObtenidas = null;
+    public List<Modelo> modelosObtenidos = null;
+    public List<AnioFabricacion> aniosObtenidos = null;
+
+    //Capaz hay que traer todo y ir filtrando a medida que elije el usuario y no hacerlo de esta manera.
+    public String[] obtenerMarcas() {
+
+        marcasObtenidas = ((new DAOMarca()).obtenerAll());
+
+        return obtenerStringMarcas(marcasObtenidas);
+    }
+
+    public String[] obtenerModelos(String marca) {
+
+        modelosObtenidos = obtenerMarcaUnica(marca).getModelos();
+
+        return obtenerStringModelos(modelosObtenidos);
+    }
+    
+        public String[] obtenerAnios(String modelo) {
+
+        aniosObtenidos = obtenerModeloUnico(modelo).getAnioModelo();
+
+        return obtenerStringAnios(aniosObtenidos);
+    }
+
+    private String[] obtenerStringMarcas(List<Marca> marc) {
+
+        String[] nombreMarcas = new String[marc.size()];
+
+        for (int i = 0; i < marc.size(); i++) {
+            nombreMarcas[i] = marc.get(i).getNombre();
         }
-        pais = new PanelDropDown(paises);
-        
-        String[] provincias = new String[listaProvincias.size()];
-        for (int i = 0; i < listaProvincias.size(); i++) {
-            provincias[i] = listaProvincias.get(i).getNombreProvincia();
+
+        return nombreMarcas;
+    }
+
+    public Marca obtenerMarcaUnica(String nombreMarca) {
+
+        Marca marcaBuscada = null;
+
+        if (marcasObtenidas == null) {
+            marcasObtenidas = ((new DAOMarca()).obtenerAll());
         }
-        provincia = new PanelDropDown(provincias);
-        
-        String[] localidades = new String[listaLocalidades.size()];
-        for (int i = 0; i < listaLocalidades.size(); i++) {
-            localidades[i] = listaLocalidades.get(i).getNombreLocalidad();
+
+        for (Marca m : marcasObtenidas) {
+            if (m.getNombre().equals(nombreMarca)) {
+                marcaBuscada = m;
+                break;
+            }
         }
-        localidad = new PanelDropDown(localidades);
+
+        return marcaBuscada;
+    }
+
+    private String[] obtenerStringModelos(List<Modelo> modelos) {
+
+        String[] nombreModelos = new String[modelos.size()];
+
+        for (int i = 0; i < modelos.size(); i++) {
+            nombreModelos[i] = modelos.get(i).getNombre();
+        }
+
+        return nombreModelos;
+    }
+
+    public Modelo obtenerModeloUnico(String nombreModelo) {
+
+        Modelo modeloBuscado = null;
+
+        for (Modelo m : modelosObtenidos) {
+            if (m.getNombre().equals(nombreModelo)) {
+                modeloBuscado = m;
+                break;
+            }
+        }
+
+        return modeloBuscado;
+    }
+
+    private String[] obtenerStringAnios(List<AnioFabricacion> anios) {
         
-        */
-        /*
-        
-        for(Provincia p: listaProvincias){    
-                    if(p.getNombreProvincia().equals(selectedItem)){
-                        listaLocalidades = p.getLocalidades();
-                        actualizarListaLocalidades();
-                        break;
-                    }
-                }
-        
-        */
-        
+        String[] stringAnios = new String[anios.size()];
+
+        for (int i = 0; i < anios.size(); i++) {
+            stringAnios[i] = anios.get(i).getAnio()+"";
+        }
+
+        return stringAnios;
+    }
+        public AnioFabricacion obtenerAnioUnico(String anio) {
+
+        AnioFabricacion anioBuscado = null;
+
+        for (AnioFabricacion a : aniosObtenidos) {
+            if ((a.getAnio()+"").equals(anio)) {
+                anioBuscado = a;
+                break;
+            }
+        }
+
+        return anioBuscado;
+    }
 }
