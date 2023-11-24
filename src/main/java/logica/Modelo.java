5a4d9c71c110de17e0ec3aefa9e9405f0dc4a58d
ValeCarlozzi <valecarlozzi@gmail.com>
2023-11-24 16:57:49 -0300

    Revert "carga de est robo"
    This reverts commit 4558d3fd9b1af1e904b9ea66925ad02ff768d505.

--- src/main/java/logica/Modelo.java
+++ src/main/java/logica/Modelo.java
@@ -2,7 +2,6 @@ package logica;
 
 import java.io.Serializable;
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -36,7 +35,7 @@ public class Modelo implements Serializable {
     @OneToMany(mappedBy = "modelo")
     private List<EstadisticaRoboVehiculo> historial;
 
-    @OneToOne(cascade = CascadeType.PERSIST)
+    @OneToOne
     private EstadisticaRoboVehiculo estadisticaActual;
 
     public Modelo() {
